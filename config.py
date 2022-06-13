3efd78f8b3bac874286d58f5236a6b78fecfc21f
trakBan <vukasinovicfran@gmail.com>
2022-06-13 17:31:34 +0200

    init

---
+++ config.py
@@ -0,0 +1,42 @@
+# This is a config file for speedreader
+
+# These are all avaliable colors (ASCII color sequences)
+black = 30
+red = 31
+green = 32
+brown = 33
+blue = 34
+purple = 35
+cyan = 36
+
+# /-----------------------\
+# |CONFIG FILE STARTS HERE|
+# \-----------------------/
+
+# WPM - words per minute
+WPM = 800
+
+# Avaliable colors are: black, red, greeen, brown, blue, purple, cyan
+# The color that highlights the middle letter
+MIDDLE_LETTER = red
+
+# The color that higlights the dot "."
+DOT = red
+
+# The color that higlights the apostrophe "'"
+APOSTROPHE = red
+
+# The color that higlights the comma ","
+COMMA = red
+
+# Pipe is the character that is below and above the world
+# The color that higlights the pipe "|"
+PIPE = red
+
+# The pipe placement "|" above and below the word
+# The higher the number is the pipe is more to the right
+PIPE_CALIBRATION = 1
+
+# /-------------------- \
+# |CONFIG FILE ENDS HERE|
+# \---------------------/
